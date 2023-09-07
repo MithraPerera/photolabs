37715f680193ab91618e358eb3e3a21f265f9761
Mithra Perera <mithraperera@hotmail.com>
2023-09-06 23:24:52 -0400

    modified photolistitem to use props passed from App

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -10,7 +10,7 @@ const PhotoListItem = (props) => {
 	const country = props.location.country;
 	/* Insert React */
 	return (
-		<div>
+		<div className='.photo-list__item'>
 			<img src={imageSource} />
 			<img src={profilePic} />
 			<p>{username}</p>
